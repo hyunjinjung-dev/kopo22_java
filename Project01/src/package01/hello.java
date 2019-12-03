a9aed420040fea14de51943f27195a8fa3af6c82
jhj1390jhj <jhj1390jhj@naver.com>
2019-12-03 10:36:54 +0900

    helloworld edit

--- Project01/src/package01/hello.java
+++ Project01/src/package01/hello.java
@@ -1,7 +1,8 @@
 package package01;
 
 public class hello {
-public static void main(String[] args) {
-	System.out.println("helloworld!");
-}
+	public static void main(String[] args) {
+		System.out.println("helloworld!");
+		System.out.println("byeworld!");
+	}
 }
